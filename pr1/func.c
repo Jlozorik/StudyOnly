63dc5ea79f48927db4d0274ecf18dc2a214e010a
Jlozorik <reshover@yandex.ru>
2025-09-11 23:16:37 +0300

    fix: func.c

--- pr1/func.c
+++ pr1/func.c
@@ -1,89 +1,47 @@
 #include <stdio.h>
 
-void task1();
-void task2();
-void task3();
-void task4();
-void task5();
-
-int main() {
-    // Задание 1
-    int num1;
+void task_1() {
+    int n, i;
+    long long factorial = 1;
     printf("Введите число для вычисления факториала: ");
-    scanf("%d", &num1);
-    long long fact = factorial(num1);
-    
-    printf("Факториал числа %d: %lld\n", num1, fact);
-    
-    printf("\n");
-
-    // Задание 2
-    int num2;
-    printf("Введите число для проверки на простоту: ");
-    scanf("%d", &num2);
-    if (isPrime(num2)) {
-        printf("Число %d является простым.\n", num2);
-    } else {
-        printf("Число %d не является простым.\n", num2);
-    }
-    printf("\n");
-
-    // Задание 3
-    int num3;
-    printf("Введите число для вывода делителей: ");
-    scanf("%d", &num3);
-    printDivisors(num3);
-    printf("\n");
-
-    // Задание 4
-    int num4_1, num4_2;
-    printf("Введите два числа: ");
-    scanf("%d %d", &num4_1, &num4_2);
-    printNumbersBetween(num4_1, num4_2);
-    printf("\n");
-
-    // Задание 5
-    int num5_1, num5_2;
-    printf("Введите два числа для проверки на дружественность: ");
-    scanf("%d %d", &num5_1, &num5_2);
-    if (areAmicable(num5_1, num5_2)) {
-        printf("Числа %d и %d являются дружественными.\n", num5_1, num5_2);
-    } else {
-        printf("Числа %d и %d не являются дружественными.\n", num5_1, num5_2);
-    }
-    
-    return 0;
-}
-
-// 1. Функция для вычисления факториала
-long long factorial(int n) {
+    scanf("%d", &n);
     if (n < 0) {
-        return -1; // Возвращаем -1 для отрицательных чисел
+        printf("Факториал отрицательного числа не существует.\n");
+    } else {
+        for (i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        printf("Факториал числа %d: %lld\n", n, factorial);
     }
-    long long result = 1;
-    for (int i = 1; i <= n; i++) {
-        result *= i;
-    }
-    return result;
 }
 
-// 2. Функция для проверки на простое число
-int isPrime(int n) {
+void task_2() {
+    int n, i, is_prime = 1;
+    printf("Введите число для проверки на простоту: ");
+    scanf("%d", &n);
     if (n <= 1) {
-        return 0; // Не является простым
-    }
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return 0; // Не является простым
+        is_prime = 0;
+    } else {
+        for (i = 2; i * i <= n; ++i) {
+            if (n % i == 0) {
+                is_prime = 0;
+                break;
+            }
         }
     }
-    return 1; // Является простым
+    if (is_prime) {
+        printf("Число %d является простым.\n", n);
+    } else {
+        printf("Число %d не является простым.\n", n);
+    }
 }
 
-// 3. Функция для вывода делителей
-void printDivisors(int n) {
+void task_3() {
+    int n, i;
+    printf("Введите число для вывода его делителей: ");
+    scanf("%d", &n);
     printf("Делители числа %d: ", n);
-    for (int i = 1; i <= n; i++) {
+    for (i = 1; i <= n; ++i) {
         if (n % i == 0) {
             printf("%d ", i);
         }
@@ -91,36 +49,62 @@ void printDivisors(int n) {
     printf("\n");
 }
 
-// 4. Функция для вывода чисел между двумя
-void printNumbersBetween(int a, int b) {
+void task_4() {
+    int a, b, i;
+    printf("Введите два числа: ");
+    scanf("%d %d", &a, &b);
     if (a > b) {
         int temp = a;
         a = b;
         b = temp;
     }
     printf("Числа между %d и %d: ", a, b);
-    for (int i = a + 1; i < b; i++) {
+    for (i = a + 1; i < b; ++i) {
         printf("%d ", i);
     }
     printf("\n");
 }
 
-// Вспомогательная функция для вычисления суммы делителей
-int sumDivisors(int n) {
-    int sum = 0;
-    for (int i = 1; i < n; i++) {
+int sum_divisors(int n) {
+    int sum = 1, i;
+    if (n <= 1) return 0;
+    for (i = 2; i * i <= n; ++i) {
         if (n % i == 0) {
             sum += i;
+            if (i * i != n) {
+                sum += n / i;
+            }
         }
     }
     return sum;
 }
 
-// 5. Функция для проверки на дружественные числа
-int areAmicable(int a, int b) {
-    if (sumDivisors(a) == b && sumDivisors(b) == a) {
-        return 1; // Являются дружественными
+void task_5() {
+    int a, b, sum_a, sum_b;
+    printf("Введите два числа для проверки на дружественность: ");
+    scanf("%d %d", &a, &b);
+    if (a == b) {
+        printf("Числа не могут быть дружественными, так как они одинаковы.\n");
+        return;
     }
-    return 0; // Не являются дружественными
+    sum_a = sum_divisors(a);
+    sum_b = sum_divisors(b);
+    if (sum_a == b && sum_b == a) {
+        printf("Числа %d и %d являются дружественными.\n", a, b);
+    } else {
+        printf("Числа %d и %d не являются дружественными.\n", a, b);
+    }
 }
 
+int main() {
+    task_1();
+    printf("\n");
+    task_2();
+    printf("\n");
+    task_3();
+    printf("\n");
+    task_4();
+    printf("\n");
+    task_5();
+    return 0;
+}
