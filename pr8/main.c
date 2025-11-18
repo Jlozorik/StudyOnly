81b73f7eb16d10c1e2f1937751c30f6a9acb1e99
Jlozorik <reshover@yandex.ru>
2025-11-18 14:52:45 +0300

    pr8 done

--- pr8/main.c
+++ pr8/main.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-#include <fcntl.h>   // Для open()
-#include <unistd.h>  // Для read(), write(), close()
-#include <stdlib.h>  // Для exit()
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    // Проверяем, что передано два имени файла
     if (argc != 3) {
-        write(STDOUT_FILENO, "Использование: ./compare file1.txt file2.txt\n", 48);
+        write(STDOUT_FILENO, "Использование: ./main file1.txt file2.txt\n", 48);
         exit(1);
     }
 
-    // Открываем файлы только для чтения
     int fd1 = open(argv[1], O_RDONLY);
     if (fd1 == -1) {
         perror("Ошибка открытия первого файла");
@@ -30,23 +28,19 @@ int main(int argc, char *argv[]) {
     long char_num = 1;
 
     while (1) {
-        // Читаем по одному байту из каждого файла
         bytes_read1 = read(fd1, &buf1, 1);
         bytes_read2 = read(fd2, &buf2, 1);
 
-        // Если один файл закончился, а другой нет
-        if (bytes_read1 != bytes_read2) {
-            write(STDOUT_FILENO, "Один файл является началом другого.\n", 60);
-            break;
-        }
-
-        // Если оба файла закончились одновременно
         if (bytes_read1 == 0 && bytes_read2 == 0) {
             write(STDOUT_FILENO, "Файлы полностью совпадают.\n", 51);
             break;
         }
 
-        // Сравниваем прочитанные байты
+        if (bytes_read1 == 0 || bytes_read2 == 0) {
+            write(STDOUT_FILENO, "Один файл является началом другого.\n", 60);
+            break;
+        }
+
         if (buf1 != buf2) {
             char buffer[100];
             int len = sprintf(buffer, "Файлы отличаются. Строка: %ld, Символ: %ld\n", line_num, char_num);
@@ -54,7 +48,6 @@ int main(int argc, char *argv[]) {
             break;
         }
 
-        // Обновляем счетчики строк и символов
         if (buf1 == '\n') {
             line_num++;
             char_num = 1;
@@ -63,7 +56,6 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Закрываем файлы
     close(fd1);
     close(fd2);
 
